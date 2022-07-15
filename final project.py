91ef6d5a85e9209eb6e2a768c2916e3a2f4ed8b9
JacksPerson <108980743+JacksPerson@users.noreply.github.com>
2022-07-15 11:35:46 -0400

    Add files via upload

---
+++ final project.py
@@ -0,0 +1,29 @@
+
+
+
+
+## Modules
+def start_window():
+    ## menu - includes 'begin investigation', 'close application', 'settings' buttons
+    ## info displayed - date and time in utc and local
+    return ##choice, date/time
+
+def investigate():
+    ## host tab ## entries - host, ip, user, username, supervisor, agency, virus notification, file path, md5 (results), process path, created, accessed, modified, notes
+    ## threat tab ## entries -  alert source, threat ID, threat IP(s), threat domain(s), notes
+    ## actions tab ## entries - contain and reimage, delete file and alert, false positive, other, notes
+
+def reimage():
+    ## Text created of reimage email, and description for ASM
+
+def settings():
+    ## adjust input settings
+
+def input_val(input_value):
+    return x ## valu code for submitted input
+
+## Main Body
+
+start_window()
+
+investigate()
